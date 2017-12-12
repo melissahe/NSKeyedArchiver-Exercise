db8c857edfd5cc6f3c8b2460ed173b12c05b39ff
Melissa He <melissahe@ac.c4q.nyc>
2017-12-12 17:17:25 -0500

    Set up partial autolayout of storyboard

---
+++ NSKeyedArchiver-Exercise/View Controllers/ImageListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ImageListViewController.swift
+//  NSKeyedArchiver-Exercise
+//
+//  Created by C4Q on 12/12/17.
+//  Copyright © 2017 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ImageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ NSKeyedArchiver-Exercise/Views/ImageTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageTableViewCell.swift
+//  NSKeyedArchiver-Exercise
+//
+//  Created by C4Q on 12/12/17.
+//  Copyright © 2017 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+  
+    @IBOutlet weak var searchImageView: UIImageView!
+    
+}
